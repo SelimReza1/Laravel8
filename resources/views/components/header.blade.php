1638c2f991dabce27d5d5d506b859bf1e09ebc53
SelimReza1 <selimreza9920@gmail.com>
2021-06-22 22:45:34 +0600

    blade component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <!-- An unexamined life is not worth living. - Socrates -->
+    <h2>Hello from header component</h2>
+    <h4>hello from {{$name}}</h4>
+    @foreach($fruits as $fruit)
+    <h5>{{$fruit}}</h5>
+    @endforeach
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index'])->name('product.index');
 
 Route::get('/home/{name?}',[HomeController::class,'index'])->name('home.index');
 Route::get('/user', [UserController::class, 'index'])->name('user.index');
