cfe72fb3bb8b0c082289018a4031546445be6668
SelimReza1 <selimreza9920@gmail.com>
2021-07-01 10:19:27 +0600

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Page Not Found</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+
+<section style="padding-top: 100px">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-2 text-center">
+                <h1 style="font-size: 140px">404</h1>
+                <h2>Page Not Found</h2>
+                <p>We are sorry, the page you requeste could not be found. Please go back to the home page</p>
+                <a href="/" class="btn btn-primary">Visit Homepage</a>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
